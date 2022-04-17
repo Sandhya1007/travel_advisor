0d21ce3468d2ebc66acd5a26846334984ef8a50e
Sandhya1007 <sandhya.vdoit@gmail.com>
2022-04-17 16:47:35 +0530

    Added dependencies and basic folder structure for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+// import { CssBaseline, Grid } from "@mui/material";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
-  return <div>Travel Advisor</div>;
+  return (
+    <div>
+      Travel Advisor
+      <Header />
+      <List />
+      <Map />
+      <PlaceDetails />
+    </div>
+  );
 };
 
 export default App;
